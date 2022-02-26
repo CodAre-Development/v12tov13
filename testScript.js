98e7ccd2bc2eafb1b11f3dceca3cb6e24ef83447
SpongeBed <56435044+SpongeBed81@users.noreply.github.com>
2022-02-26 18:41:17 +0300

    Update testScript.js

--- testScript.js
+++ testScript.js
@@ -1,3 +1,8 @@
+
 const collector = message.createReactionCollector(filter, { time: 15000 });
+const reactions = await message.awaitReactions(filter, { time: 15000 });
+console.log(guild.ownerID)
 
+client.on(`message`, () => {
 
+})
