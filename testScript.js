e1cd4d9ca7c533acd75a5ef52900d56b3aadf76f
SpongeBed <56435044+SpongeBed81@users.noreply.github.com>
2022-03-12 14:38:54 +0300

    Update testScript.js

--- testScript.js
+++ testScript.js
@@ -2,7 +2,10 @@
 const collector = message.createReactionCollector(filter, { time: 15000 });
 const reactions = await message.awaitReactions(filter, { time: 15000 });
 console.log(guild.ownerID)
-
+client.user.setPresence({ activity: { name: 'with discord.js' } }, {merhaba: "napcan"});
 client.on(`message`, () => {
 
 })
+client.fetchApplication()
+client.fetchWidget()
+client.generateInvite([Permissions.FLAGS.SEND_MESSAGES]);
